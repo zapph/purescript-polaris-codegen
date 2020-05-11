ee73c9f4cbd1d9c2e8b534f0872abf7a6d57c934
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-05-11 16:50:10 +0800

    Add replacements for type name conflicts

--- src/Polaris/Codegen/PrinterUtils.purs
+++ src/Polaris/Codegen/PrinterUtils.purs
@@ -6,14 +6,24 @@ module Polaris.Codegen.PrinterUtils
 import Prelude
 
 import Data.Array as Array
+import Data.Maybe (fromMaybe)
 import Data.String (Pattern(..), Replacement(..))
 import Data.String as String
 import Data.String.Extra (pascalCase)
+import Foreign.Object (Object)
+import Foreign.Object as Object
 
 printRefName :: Array String -> String
-printRefName ns =
-  Array.intercalate "__"
-  $ String.replaceAll (Pattern ".") (Replacement "_") <<< pascalCase <$> ns
+printRefName ns = fromMaybe name (Object.lookup name replacements)
+  where
+    name =
+      Array.intercalate "__"
+      $ String.replaceAll (Pattern ".") (Replacement "_") <<< pascalCase <$> ns
 
 lines :: Array String -> String
 lines = Array.intercalate "\n"
+
+replacements :: Object String
+replacements = Object.fromHomogeneous
+  { "Array": "PArray"
+  }
